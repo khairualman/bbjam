c0f29ac630a671dd5d0e5df747f94cb829e2e420
ina <ina@tapquo.com>
2012-11-17 13:28:18 +0100

    Drum start

--- js/Instrument.Drum.js
+++ js/Instrument.Drum.js
@@ -1,8 +1,37 @@
 Instrument.Drum = function() {
 
+    var sounds = [
+        'chh.ogg',
+        'cow.ogg',
+        'kick.ogg',
+        'ohh.ogg',
+        'ride.ogg',
+        'snare.ogg'
+    ];
+
+
+    var _addPadsToBody = function() {
+        var body, sounds, audio_html, j, i;
+        body = $$("body");
+        for(i in sounds) {
+            sound_ogg = sounds[i];
+            for(j = 0; j < 4; j++) {
+                pad_html = '<audio src="drumsamples/' + sound_ogg + '" id="' + sound_ogg + '"></audio>';
+                pad_html += '<div class="drum_pad" id="' + sound_ogg + '">' + sound_ogg + '</div>';
+                body.append(pad_html);
+            }
+        }
+    }:
 
 
 
+    var init = function() {
+        _addPadsToBody();
+    };
 
 
-}
+    return {
+        init: init
+    };
+
+};
--- js/Instrument.Keyboard.js
+++ js/Instrument.Keyboard.js
@@ -5,4 +5,4 @@ Instrument.Keyboard = function() {
 
 
 
-}
+};
